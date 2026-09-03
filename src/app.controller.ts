7a1168a7e7a3d0cb40bc52c911667c4f5317c23b
Alejandro Perez <perezalejandrojulian@gmail.com>
2026-09-03 20:32:59 -0300

    [DYA-2] Add hello endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service.js';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('')
+  sayHello(): string {
+    return "Delivery API";
+  }
+
   @Get('/health')
   health(): string {
     return this.appService.healthCheck();
