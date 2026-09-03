5e8079e0a6ac15fab57569491046d62157db5afa
Alejandro Perez <perezalejandrojulian@gmail.com>
2026-09-03 19:37:55 -0300

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
