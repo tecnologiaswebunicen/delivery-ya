f1d9673ca72f2c9c6f065cbef70fa19d5ebba120
Alejandro Perez <perezalejandrojulian@gmail.com>
2026-09-10 20:24:49 -0300

    asdasd

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -20,7 +20,7 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Delivery API');
+      .expect('asdasda API');
   });
 
   afterEach(async () => {
